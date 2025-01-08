0f5d54a515dfa94d195045196e5ca2c793b14c27
Aboubacar Djenika <djenikaa@gmail.com>
2025-01-08 12:23:02 +0000

    Importing project

---
+++ docs/source/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CID CoreBanking'
+copyright = '2025, THL Technologies'
+author = 'THL Technologies'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+def setup(app):
+    app.add_js_file('js/target-blank.js')
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_permalinks_icon = '<span>#</span>'
+html_theme = 'sphinxawesome_theme'
+html_static_path = ['_static']
